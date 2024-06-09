97aafb9b5cc7d175dabc2c69678c7aece7965323
akifkemalaksu <akifkemalaksu@gmail.com>
2024-06-09 22:32:01 +0300

    added lock operation.

--- Integration/Service/ItemIntegrationService.cs
+++ Integration/Service/ItemIntegrationService.cs
@@ -1,5 +1,6 @@
 ﻿using Integration.Common;
 using Integration.Backend;
+using System.Collections.Concurrent;
 
 namespace Integration.Service;
 
@@ -8,21 +9,35 @@ public sealed class ItemIntegrationService
     //This is a dependency that is normally fulfilled externally.
     private ItemOperationBackend ItemIntegrationBackend { get; set; } = new();
 
+    private static readonly ConcurrentDictionary<string, object> processingItems = new ConcurrentDictionary<string, object>();
+
     // This is called externally and can be called multithreaded, in parallel.
     // More than one item with the same content should not be saved. However,
     // calling this with different contents at the same time is OK, and should
     // be allowed for performance reasons.
     public Result SaveItem(string itemContent)
     {
-        // Check the backend to see if the content is already saved.
-        if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+        var lockObject = processingItems.GetOrAdd(itemContent, new object());
+
+        lock (lockObject)
         {
-            return new Result(false, $"Duplicate item received with content {itemContent}.");
+            try
+            {
+                // Check the backend to see if the content is already saved.
+                if (ItemIntegrationBackend.FindItemsWithContent(itemContent).Count != 0)
+                {
+                    return new Result(false, $"Duplicate item received with content {itemContent}.");
+                }
+
+                var item = ItemIntegrationBackend.SaveItem(itemContent);
+
+                return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
+            }
+            finally
+            {
+                processingItems.TryRemove(itemContent, out _);
+            }
         }
-
-        var item = ItemIntegrationBackend.SaveItem(itemContent);
-
-        return new Result(true, $"Item with content {itemContent} saved with id {item.Id}");
     }
 
     public List<Item> GetAllItems()
